a37bdb496b6ce7fef6689825fffa234adf7fbc91
SKim041 <sehee1806@gmail.com>
2025-06-26 17:33:02 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.06 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+// 모든 달은 28일까지 
+vector<int> solution(string today, vector<string> terms, vector<string> privacies) {
+    vector<int> answer;
+    unordered_map<string, int> mapTerms;
+    // today의 총 일수
+    int t_totalDays = stoi(today.substr(0, 4))*12*28 
+        + stoi(today.substr(5, 2))*28 + stoi(today.substr(8, 2));
+    // terms map에 넣기
+    for (auto s : terms)
+    {
+        mapTerms[s.substr(0, 1)] = stoi(s.substr(2));
+    }
+
+    for (int i = 0; i < privacies.size(); i++)
+    {
+        string term = privacies[i].substr(11);
+        int YYYY = stoi(privacies[i].substr(0, 4));
+        int MM = stoi(privacies[i].substr(5, 2));
+        int DD = stoi(privacies[i].substr(8, 2));
+        
+        int term_totalDays = YYYY*12*28 + (MM+mapTerms[term])*28 + DD - 1;
+        
+        if (t_totalDays > term_totalDays)
+        {
+            answer.push_back(i + 1);
+        }
+    }
+    return answer;
+}
